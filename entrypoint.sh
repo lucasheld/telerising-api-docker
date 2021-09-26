5b4365dfcf17246e51a79fefd7e243b72120f454
lucasheld <lucasheld@hotmail.de>
2021-09-26 13:45:26 +0200

    fix config permission issues

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
-chown -R ${PUID}:${PGID} /app /config
 if [ -f /config/settings.json ]
 then
     ln -s /config/settings.json /app/settings.json
 else
-    echo '{}' > /app/settings.json
-    ln -s /app/settings.json /config/settings.json
+    echo '{}' > /config/settings.json
+    ln -s /config/settings.json /app/settings.json
 fi
+
+chown -R ${PUID}:${PGID} /app /config
 
 cd /app
 su-exec ${PUID}:${PGID} ./api

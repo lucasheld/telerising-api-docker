19052fe8debc3d3074ea760cdeb970b4acbdd900
Lucas Held <lucasheld@hotmail.de>
2025-07-26 18:05:50 +0200

    symlink all files from /config to /app

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,17 @@ if [ ! -f /config/settings.json ]
 then
     echo '{}' > /config/settings.json
 fi
-ln -s /config/settings.json /app/settings.json
+
+src_base="/config"
+dst_base="/app"
+find "$src_base" -type f | while read -r src_file; do
+    rel_path="${src_file#$src_base/}"
+    target_path="$dst_base/$rel_path"
+    target_dir=$(dirname "$target_path")
+    mkdir -p "$target_dir"
+    rm -f "$target_path"
+    ln -s "$src_file" "$target_path"
+done
 
 chown -R ${PUID}:${PGID} /app /config
 
